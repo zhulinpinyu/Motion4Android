798a7e297c027a2d5b4bb6f9b05db95f0857aed2
zhulinpinyu <mulixiang199016@gmail.com>
2015-06-30 17:04:39 +0800

    show text in activity

--- Colorful/app/main_activity.rb
+++ Colorful/app/main_activity.rb
@@ -1,5 +1,10 @@
 class MainActivity < Android::App::Activity
   def onCreate(savedInstanceState)
     super
+    @text = Android::Widget::TextView.new(self)
+    @text.text = "Hi RubyMotion for Android"
+    @text.textColor = Android::Graphics::Color::GREEN
+    @text.textSize = 36.0
+    self.contentView = @text
   end
 end
